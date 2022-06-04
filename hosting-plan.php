ac33724510f2e90949924a0c6400179624af881a
DollyPro - Digital Presence <106308569+DollyPro@users.noreply.github.com>
2022-06-04 20:11:41 +0300

    Delete hosting-plan.php

--- hosting-plan.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hosting_finish_date = new DateTime("2022-09-20");
-//Do Not Edit
-$date = new DateTime("now", new DateTimeZone('Asia/Beirut') );
-$difference = $date->diff($hosting_finish_date);
-$days_left = $difference->d + ( $difference->m * 30 ) + ( $difference->y * 365 ) ;
-if ( ($date>$hosting_finish_date) ) die("Expired Hosting Plan");
-//Do Not Edit
-?>
